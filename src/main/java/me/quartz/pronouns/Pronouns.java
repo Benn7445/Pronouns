b9d310cf6daf5410a3059847a682476a913ef36d
Ben Verbraecken <ben.verbraecken@gmail.com>
2023-11-19 23:22:20 +0100

    Start

---
+++ src/main/java/me/quartz/pronouns/Pronouns.java
@@ -0,0 +1,16 @@
+package me.quartz.pronouns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pronouns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
